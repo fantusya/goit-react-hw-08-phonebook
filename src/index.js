071c5272753328d417b33aa0851cdea4ec2614cb
Anastasiia <guba.anastaysha@gmail.com>
2022-12-23 12:24:35 +0200

    added persist gate and browser router

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './redux/store';
 import 'react-toastify/dist/ReactToastify.css';
 
 import './index.css';
-import { store } from './redux/store';
 import App from 'components/App';
 import { theme } from './constants/theme';
 
@@ -13,7 +15,11 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <Provider store={store}>
-        <App />
+        <PersistGate loading={null} persistor={persistor}>
+          <BrowserRouter basename="/goit-react-hw-08-phonebook">
+            <App />
+          </BrowserRouter>
+        </PersistGate>
       </Provider>
     </ThemeProvider>
   </React.StrictMode>
