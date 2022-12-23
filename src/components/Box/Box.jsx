0bbc4ddc958f780d30ed97e21c09148262937b3b
Anastasiia <guba.anastaysha@gmail.com>
2022-12-23 12:13:15 +0200

    added typography

--- src/components/Box/Box.jsx
+++ src/components/Box/Box.jsx
@@ -1,4 +1,4 @@
 import styled from 'styled-components';
-import { space, layout, flexbox, grid } from 'styled-system';
+import { space, layout, flexbox, grid, typography } from 'styled-system';
 
-export const Box = styled('div')(space, layout, flexbox, grid);
+export const Box = styled('div')(space, layout, flexbox, grid, typography);
