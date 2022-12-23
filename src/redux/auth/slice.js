a280a11ee9f311c469cac41b638cde6c3c64d600
Anastasiia <guba.anastaysha@gmail.com>
2022-12-23 12:22:11 +0200

    created redux slice for authorization

---
+++ src/redux/auth/slice.js
@@ -0,0 +1,65 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { register, logIn, logOut, refreshUser } from './operations';
+
+const handlePending = state => {
+  state.error = false;
+};
+
+const handleRejected = state => {
+  state.isLoading = false;
+  state.error = true;
+};
+
+const initialState = {
+  user: { name: null, email: null },
+  token: null,
+  isLoggedIn: false,
+  isRefreshing: false,
+  error: null,
+};
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  extraReducers: {
+    [register.fulfilled](state, action) {
+      console.log(action);
+      state.user = action.payload.user;
+      state.token = action.payload.token;
+      state.isLoggedIn = true;
+      state.error = false;
+    },
+    [logIn.fulfilled](state, action) {
+      state.user = action.payload.user;
+      state.token = action.payload.token;
+      state.isLoggedIn = true;
+      state.error = false;
+    },
+    [logOut.fulfilled](state) {
+      state.user = { name: null, email: null };
+      state.token = null;
+      state.isLoggedIn = false;
+      state.error = false;
+    },
+    [refreshUser.fulfilled](state, action) {
+      state.user = action.payload;
+      state.isLoggedIn = true;
+      state.isRefreshing = false;
+      state.error = false;
+    },
+    [register.pending]: handlePending,
+    [logIn.pending]: handlePending,
+    [logOut.pending]: handlePending,
+    [refreshUser.pending](state) {
+      state.isRefreshing = true;
+    },
+    [register.rejected]: handleRejected,
+    [logIn.rejected]: handleRejected,
+    [logOut.rejected]: handleRejected,
+    [refreshUser.rejected](state) {
+      state.isRefreshing = false;
+    },
+  },
+});
+
+export const authReducer = authSlice.reducer;
