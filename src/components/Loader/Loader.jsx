32e5eb083df0e28a410cf7089f770ab05c913d22
Anastasiia <guba.anastaysha@gmail.com>
2022-12-23 12:13:32 +0200

    added size prop

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,11 +1,11 @@
 import { ColorRing } from 'react-loader-spinner';
 
-const Loader = () => {
+const Loader = ({ sizeValue }) => {
   return (
     <ColorRing
       visible={true}
-      height="40"
-      width="40"
+      height={sizeValue}
+      width={sizeValue}
       ariaLabel="blocks-loading"
       wrapperClass="blocks-wrapper"
       colors={['#8185ce', '#B2A3B5', '#7773a8', '#51E5FF', '#429EA6']}
