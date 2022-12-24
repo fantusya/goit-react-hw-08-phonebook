70f3b6cf99e54741b5b5ed6de0e1b64629a32d4d
Anastasiia <guba.anastaysha@gmail.com>
2022-12-24 13:14:57 +0200

    deleted error selector from hook

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -3,19 +3,16 @@ import {
   selectUser,
   selectIsLoggedIn,
   selectIsRefreshing,
-  selectIsError,
 } from 'redux/auth/selectors';
 
 export const useAuth = () => {
   const isLoggedIn = useSelector(selectIsLoggedIn);
   const isRefreshing = useSelector(selectIsRefreshing);
   const user = useSelector(selectUser);
-  const error = useSelector(selectIsError);
 
   return {
     isLoggedIn,
     isRefreshing,
     user,
-    error,
   };
 };
