74b7e6cefdd6546cc1814079e8242d7343aa22aa
Anastasiia <guba.anastaysha@gmail.com>
2022-12-24 13:14:04 +0200

    deleted error handling in the auth slice

--- src/redux/auth/slice.js
+++ src/redux/auth/slice.js
@@ -5,17 +5,11 @@ const handlePending = state => {
   state.error = false;
 };
 
-// const handleRejected = state => {
-//   state.isLoading = false;
-//   state.error = true;
-// };
-
 const initialState = {
   user: { name: null, email: null },
   token: null,
   isLoggedIn: false,
   isRefreshing: false,
-  error: null,
 };
 
 const authSlice = createSlice({
@@ -27,25 +21,21 @@ const authSlice = createSlice({
       state.user = action.payload.user;
       state.token = action.payload.token;
       state.isLoggedIn = true;
-      // state.error = false;
     },
     [logIn.fulfilled](state, action) {
       state.user = action.payload.user;
       state.token = action.payload.token;
       state.isLoggedIn = true;
-      // state.error = false;
     },
     [logOut.fulfilled](state) {
       state.user = { name: null, email: null };
       state.token = null;
       state.isLoggedIn = false;
-      // state.error = false;
     },
     [refreshUser.fulfilled](state, action) {
       state.user = action.payload;
       state.isLoggedIn = true;
       state.isRefreshing = false;
-      // state.error = false;
     },
     [register.pending]: handlePending,
     [logIn.pending]: handlePending,
@@ -53,9 +43,6 @@ const authSlice = createSlice({
     [refreshUser.pending](state) {
       state.isRefreshing = true;
     },
-    // [register.rejected]: handleRejected,
-    // [logIn.rejected]: handleRejected,
-    // [logOut.rejected]: handleRejected,
     [refreshUser.rejected](state) {
       state.isRefreshing = false;
     },
