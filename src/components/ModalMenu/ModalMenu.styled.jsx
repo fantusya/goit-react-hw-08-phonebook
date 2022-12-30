ee0c1b7eb106964e2639f7ef9549b98b7ff569f5
Anastasiia <guba.anastaysha@gmail.com>
2022-12-30 18:21:08 +0200

    added animatiom for emersion

--- src/components/ModalMenu/ModalMenu.styled.jsx
+++ src/components/ModalMenu/ModalMenu.styled.jsx
@@ -19,4 +19,17 @@ export const ModalContent = styled.div`
   border-bottom: ${p => p.theme.borders.normal} ${p => p.theme.colors.silver};
 
   background-color: ${p => p.theme.colors.modalBg};
+
+  animation: modal-emersion 0.5s cubic-bezier(0.25, 0.46, 0.45, 0.94) both;
+
+  @keyframes modal-emersion {
+    0% {
+      transform: translateY(-100%);
+      opacity: 1;
+    }
+    100% {
+      transform: translateY(0%);
+      opacity: 1;
+    }
+  }
 `;
