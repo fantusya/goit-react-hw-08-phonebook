759d7ac4022ce94b70a172703265daca8fe3cb19
Anastasiia <guba.anastaysha@gmail.com>
2023-01-02 15:14:25 +0200

    added container and paddings to a section

--- src/pages/ContactsPage/ContactsPage.jsx
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -2,6 +2,7 @@ import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { Box } from 'components/Box/Box';
 
+import { Container } from 'constants/GlobalStyle';
 import { PageTitle, ContactsTitle } from './ContactsPage.styled';
 import { fetchContacts } from 'redux/contacts/operations';
 import ContactForm from 'components/ContactForm';
@@ -16,37 +17,49 @@ const ContactsPage = () => {
   }, [dispatch]);
 
   return (
-    <Box
-      pt={5}
-      pl={7}
-      display="flex"
-      flexDirection="column"
-      alignItems="flex-start"
-      justifyContent="space-around"
-      gridGap={4}
-    >
-      <PageTitle>Phonebook</PageTitle>
-      <Box
-        display="inline-flex"
-        alignItems="flex-start"
-        justifyContent="space-around"
-        gridGap="200px"
-      >
-        <Box
-          display="inline-flex"
-          flexDirection="column"
-          alignItems="center"
-          justifyContent="center"
-        >
-          <ContactForm />
-        </Box>
-        <Box>
-          <ContactsTitle>Contacts</ContactsTitle>
-          <Filter />
-          <ContactList />
-        </Box>
+    <main>
+      <Box as="section" py={5}>
+        <Container>
+          <Box
+            pl={[null, null, null, null, '50px']}
+            display={['block', null, null, null, 'flex']}
+            flexDirection="column"
+            alignItems="flex-start"
+            justifyContent="space-around"
+            gridGap={5}
+            textAlign="center"
+          >
+            <PageTitle>Phonebook</PageTitle>
+            <Box
+              display={[null, null, null, null, 'inline-flex']}
+              flexDirection={['column', null, null, null, 'row']}
+              alignItems="flex-start"
+              justifyContent="space-around"
+              gridGap={['50px', null, null, null, '150px']}
+            >
+              <Box
+                // display="inline-flex"
+                // flexDirection="column"
+                // alignItems="center"
+                // justifyContent="center"
+                mb={[5, null, null, null, 0]}
+              >
+                <ContactForm />
+              </Box>
+              <Box
+                display="inline-flex"
+                flexDirection="column"
+                alignItems={['center', null, null, null, 'flex-start']}
+              >
+                <ContactsTitle>Contacts</ContactsTitle>
+                <Filter />
+                <ContactList />
+              </Box>
+            </Box>
+          </Box>
+        </Container>
       </Box>
-    </Box>
+    </main>
   );
 };
 
