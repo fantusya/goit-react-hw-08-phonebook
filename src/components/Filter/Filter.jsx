3bd22a1d701c88d9551475c6bb2bf11de5d49150
Anastasiia <guba.anastaysha@gmail.com>
2023-01-02 15:15:14 +0200

    added Box to component for styling

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Box } from 'components/Box/Box';
 import { useDispatch } from 'react-redux';
 import { setFilter } from 'redux/contacts/filterSlice';
 
@@ -13,10 +14,16 @@ const Filter = () => {
   const handleFilter = e => dispatch(setFilter(e.currentTarget.value));
 
   return (
-    <FilterLabelContainer>
+    <Box
+      display={'flex'}
+      flexDirection="column"
+      alignItems={['center', null, null, null, 'flex-start']}
+      gridGap={4}
+      mb={5}
+    >
       <FilterLabel>Find contacts by name</FilterLabel>
       <FilterInput type="text" onChange={handleFilter} />
-    </FilterLabelContainer>
+    </Box>
   );
 };
 
