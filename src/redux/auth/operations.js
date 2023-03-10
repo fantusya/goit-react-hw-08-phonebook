b0885319614f9a4bd778a248051827862067ba7e
Anastasiia <100998437+fantusya@users.noreply.github.com>
2023-03-10 11:05:53 +0200

    uncommented url

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-// axios.defaults.baseURL = 'https://my-phonebook-app.herokuapp.com';
-axios.defaults.baseURL = 'http://localhost:3000';
+axios.defaults.baseURL = 'https://my-phonebook-app.herokuapp.com';
+// axios.defaults.baseURL = 'http://localhost:3000';
 
 const token = {
   set(token) {
--- src/redux/contacts/operations.js
+++ src/redux/contacts/operations.js
@@ -7,8 +7,8 @@ import axios from 'axios';
 //   fetchingError,
 // } from './contactsSlice';
 
-// axios.defaults.baseURL = 'https://my-phonebook-app.herokuapp.com';
-axios.defaults.baseURL = 'http://localhost:3000';
+axios.defaults.baseURL = 'https://my-phonebook-app.herokuapp.com';
+// axios.defaults.baseURL = 'http://localhost:3000';
 
 // export const fetchContacts = () => async dispatch => {
 //   try {
