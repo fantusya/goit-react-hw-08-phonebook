ebec7b520c242e8de6295fb334711f91764076ae
fantusya <100998437+fantusya@users.noreply.github.com>
2023-03-10 08:08:08 +0000

    Deploying to gh-pages from @ fantusya/goit-react-hw-08-phonebook@be6962c3326a18b28e03b34dbfdc9a2554575be6 🚀

---
+++ static/js/425.fa518b5b.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[425],{7425:function(t,a,n){n.d(a,{jpy:function(){return h}});var e=n(9983);function h(t){return(0,e.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"g",attr:{},child:[{tag:"path",attr:{fill:"none",d:"M0 0h24v24H0z"}},{tag:"path",attr:{d:"M21 2C6 2 4 16 3 22h1.998c.666-3.333 2.333-5.166 5.002-5.5 4-.5 7-4 8-7l-1.5-1 1-1c1-1 2.004-2.5 3.5-5.5z"}}]}]})(t)}}}]);
+//# sourceMappingURL=425.fa518b5b.chunk.js.map
